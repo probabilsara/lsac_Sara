407913af828ad0b1b0175ca6dc7663699b43727c
probabilsara <sara.cirje23@gmail.com>
2024-12-09 16:35:41 +0200

    removed vite

--- frontend/src/script.jsx
+++
@@ -1,8 +0,0 @@
-// Get the Login button by class name
-let loginButton = document.querySelector('.Login');
-
-// Add an event listener to the Login button
-loginButton.addEventListener('click', function() {
-    // Redirect to another page when the Login button is clicked
-    window.location.href = "sign in.html"; // Replace with the URL of your login page
-});
